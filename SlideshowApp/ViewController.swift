71a2e53505cf452e3d8bcecbb5cd207621a80d08
rino.yamamoto <rino.matsushima@gmail.com>
2022-02-08 20:28:29 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 山本梨野 on 2022/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
